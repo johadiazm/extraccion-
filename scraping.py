41987cc568a606e195e902aff37a1bcbd959a5e0
andres-sanabria-10 <andres.sanabria02@uptc.edu.co>
2024-09-22 11:25:36 -0500

    ojitos

--- scraping.py
+++ scraping.py
@@ -212,6 +212,7 @@ def info_grupo_publicaciones(link_grupo):
 
 # Función para limpiar texto de caracteres no válidos para Excel
 def limpiar_texto(texto):
+    
     # Elimina caracteres no imprimibles
     texto = re.sub(r'[\x00-\x1F\x7F-\x9F]', '', texto)
     # Elimina espacios múltiples
